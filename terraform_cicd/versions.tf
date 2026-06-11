6bd90802ed929d09a708a20dbcccbfcbb87b2a57
mattia.borrini <mattia.borrini@adesso.ch>
2026-06-11 16:12:26 +0200

    Fix ci

--- terraform_cicd/versions.tf
+++ terraform_cicd/versions.tf
@@ -2,22 +2,6 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "tf-state"
-    key            = "${var.environment}/terraform.tfstate"
-    region         = "eu-central-1"
-
-    dynamodb_table = "tf-state-lock"
-
-    endpoints = {
-      s3       = "http://localhost:4566"
-      dynamodb = "http://localhost:4566"
-    }
-
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-
-    use_path_style = true
   }
 
   required_providers {
